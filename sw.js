723310dd6248a5f580583dd4a208d86944a16591
akunssh0-beep <akunssh0@gmail.com>
2026-05-06 08:05:45 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+
+const CACHE_NAME = 'jiwacare-v1778025830400';
+const ASSETS = ["./","./manifest.json","./icon-192.png","./icon-512.png","./index.html"];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
+self.addEventListener('fetch', e => {
+    if (e.request.url.includes('script.google.com')) return fetch(e.request);
+    e.respondWith(caches.match(e.request).then(response => {
+            const fetchPromise = fetch(e.request).then(networkResponse => {
+                if (networkResponse.status === 200) {
+                    const responseClone = networkResponse.clone();
+                    caches.open(CACHE_NAME).then(cache => { cache.put(e.request, responseClone); });
+                }
+                return networkResponse;
+            });
+            return response || fetchPromise;
+        })
+    );
+});
+self.addEventListener('sync', e => { if (e.tag === 'sync-absensi') e.waitUntil(console.log('Syncing...')); });
